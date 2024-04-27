3300897948a8ff7717d06f6020fd8f35c28a2e27
Manimonan <167786934+Manimonan@users.noreply.github.com>
2024-04-27 06:00:37 +0000

    ok

---
+++ Scope.js
@@ -0,0 +1,15 @@
+// Scope are tow types - Global Scope , local Scope.
+// 'var' have global scope 
+// 'let' and 'cnst' are local scope
+
+let a = 30;
+if (true){
+    let a = 300;
+   // console.log(" Inear :",a);
+}
+//console.log( a)
+
+// Nexted Scope
+
+
+
