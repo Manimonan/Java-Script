c6465a8f1a1d032b37d3aefd8467e2f4e2fda044
Manimonan <167786934+Manimonan@users.noreply.github.com>
2024-04-27 04:51:35 +0000

    ok

--- Function.js
+++ Function.js
@@ -1,4 +1,4 @@
-// Add tow numbers
+// +++++++++.   Add tow numbers. +++++++++
 
 function addTowNum( number1 , number2){
    // console.log( number1 + number2)
@@ -14,5 +14,72 @@ function addTowNum( number3,number4 ){
    return number3 + number4
 }
 const Result = addTowNum( 50 , 65 )
-console.log(Result )
+//console.log(Result )
 
+// +++++++++.     string interpulation using function. +++++++++
+
+function UserLoggin ( Username){
+       return ` loggedin user name is ${ Username}`
+}
+ //console.log(UserLoggin( " Manimohan"))
+// console.log(UserLoggin( )). [ if we pass empty value the result is undefined]
+ // In that case we check the value of " Username"
+
+ function UserLoggin ( Username){
+   if( Username === undefined){ // this also define  !Username
+      //console.log(' please put your username');
+      return
+   }
+   return ` loggedin user name is ${ Username}`
+ }
+// console.log(UserLoggin( ))
+
+ // +++++  How to pass " rest oparetor " in a function // use to convart multiple data to a arry  ++++
+
+function calculatecartPrice( ...value1 ){
+        return value1
+ }
+//console.log(calculatecartPrice(400,300 ,500,700));
+
+// ++++   How to pass Object in a function ++++ 
+
+const CardDitils = {
+    Type: " T- Shart",
+    Size: " M ",
+    Price : 999
+}
+function HandelObject( myObject){
+  // return console.log(` My ${ myObject.Type} size is ${ myObject.Size} and this price was ${ myObject.Price}`);
+}
+HandelObject( CardDitils);
+
+
+// Another way 
+
+function HandelObject1( myObject1){
+ //  return console.log(` My ${ myObject1.Type} size is ${ myObject1.Size} and this price was ${ myObject1.Price}`);
+}
+HandelObject1( 
+   {
+    Type: " Shart",
+    Size: " M ",
+    Price : 1499
+   }
+);
+
+// ++++   How to pass Array in a function ++++ 
+ 
+const MyArray = [ 200,400, 600, " Manimohan"]
+ function HandelArray( thisArray){
+    return thisArray[ 3]
+ }
+// console.log( HandelArray(MyArray));
+
+// another way
+
+function HandelArray1( thisArray){
+   return thisArray[ 2]
+}
+console.log( HandelArray1(
+   [ 200,400, 600, " Manimohan"]
+));
