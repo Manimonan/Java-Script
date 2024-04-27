4e7f977f741d83271d6a5cdc1226719d4fb9e7d2
Manimonan <167786934+Manimonan@users.noreply.github.com>
2024-04-27 03:22:52 +0000

    ok

---
+++ Function.js
@@ -0,0 +1,18 @@
+// Add tow numbers
+
+function addTowNum( number1 , number2){
+   // console.log( number1 + number2)
+}
+//addTowNum( 45 ," name")
+
+// Result stroed in a variable
+
+function addTowNum( number3,number4 ){
+    //const Result = number3+number4
+   // return Result
+              // Another process.    after return statement  any valus are not executed
+   return number3 + number4
+}
+const Result = addTowNum( 50 , 65 )
+console.log(Result )
+
